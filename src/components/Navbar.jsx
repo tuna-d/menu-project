a689eabe05db1c8c99030eb108f3111a45fffa61
tuna-d <tdemirel98@gmail.com>
2025-07-23 18:44:40 +0300

    Add navbar component.

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,28 @@
+import { Link } from "react-router"
+
+export default function Navbar({ path }) {
+  return (
+    <nav className="bg-white p-4 fixed top-0 left-0 w-full z-10 flex items-center  ">
+      {path !== "/" && (
+        <button className="absolute active:scale-90 ease-in-out duration-100">
+          <Link to={path}>
+            <img
+              width="44"
+              height="44"
+              src="https://img.icons8.com/laces/64/left.png"
+              alt="left"
+            />
+          </Link>
+        </button>
+      )}
+
+      <div className="mx-auto">
+        <img
+          src="src/assets/images/bunch_logo.png"
+          alt="logo"
+          className="h-8 m-1.5"
+        />
+      </div>
+    </nav>
+  )
+}
---
+++ src/data/categories.js
@@ -0,0 +1,107 @@
+export default categories = [
+  {
+    name: "Yemek",
+    image:
+      "https://images.unsplash.com/photo-1542528180-a1208c5169a5?q=80&w=2077&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+  {
+    name: "Tatlı",
+    image:
+      "https://images.unsplash.com/photo-1506459225024-1428097a7e18?q=80&w=687&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+  {
+    name: "Şişe Bira",
+    image:
+      "https://images.unsplash.com/photo-1587118885707-9cdd9acfcc5f?q=80&w=1171&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+  {
+    name: "Fıçı Bira",
+    image:
+      "https://images.unsplash.com/photo-1441985969846-3e7c90531139?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+  {
+    name: "Konyak",
+    image:
+      "https://images.unsplash.com/photo-1692812526027-b84ddba37afb?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+  {
+    name: "Likör",
+    image:
+      "https://images.unsplash.com/photo-1616409780339-9a91febdbb03?q=80&w=1171&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+  {
+    name: "Mezcal",
+    image:
+      "https://images.unsplash.com/photo-1670289064698-09f2c2368474?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+  {
+    name: "Sıcak İçecek",
+    image:
+      "https://images.unsplash.com/photo-1515442261605-65987783cb6a?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+  {
+    name: "Soğuk İçecek",
+    image:
+      "https://images.unsplash.com/photo-1711154319702-70f9e8c3a90f?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+  {
+    name: "Çerez",
+    image:
+      "https://images.unsplash.com/photo-1626697556426-8a55a8af4999?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+  {
+    name: "Bunch The Cocktails",
+    image:
+      "https://images.unsplash.com/photo-1551024709-8f23befc6f87?q=80&w=1257&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+  {
+    name: "Klasik Kokteyl",
+    image:
+      "https://images.unsplash.com/photo-1558383409-ab7ef8db3e01?q=80&w=1193&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+  {
+    name: "Kırmızı Şarap",
+    image:
+      "https://images.unsplash.com/photo-1506377247377-2a5b3b417ebb?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+  {
+    name: "Beyaz Şarap",
+    image:
+      "https://images.unsplash.com/photo-1498429152472-9a433d9ddf3b?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+  {
+    name: "Roze Şarap",
+    image:
+      "https://images.unsplash.com/photo-1623664506751-d0547d5d99d6?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+  {
+    name: "Köpüklü Şarap",
+    image:
+      "https://images.unsplash.com/photo-1546567075-d7113bee3c4a?q=80&w=1097&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+  {
+    name: "Shot",
+    image:
+      "https://images.unsplash.com/photo-1608635360956-5ebde7952ed6?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+  {
+    name: "Viski",
+    image:
+      "https://images.unsplash.com/photo-1585703164392-171d7f8a0af5?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+  {
+    name: "Cin",
+    image:
+      "https://images.unsplash.com/photo-1520935443738-cdd39913a9e4?q=80&w=1137&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+  {
+    name: "Votka",
+    image:
+      "https://images.unsplash.com/photo-1516758288207-e9059e0b3b3f?q=80&w=1171&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+  {
+    name: "Rom",
+    image:
+      "https://images.unsplash.com/photo-1626167344466-54fde4540a9d?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+]
